587d5813bc720c0346d1ad573937c8bca2b0fbb1
Mark Meysenburg <mark.meysenburg@doane.edu>
2023-12-01 11:00:07 -0600

    Update difference-engine.js to fix infinite loop

--- assets/js/difference-engine.js
+++ assets/js/difference-engine.js
@@ -1,11 +1,11 @@
 function submit() {
   let table = document.getElementById("tblFD");
-  let f0 = document.getElementById("inpF0").value;
-  let d1 = document.getElementById("inpD1").value;
-  let d2 = document.getElementById("inpD2").value;
-  let d3 = document.getElementById("inpD3").value;
-  let d4 = document.getElementById("inpD4").value;
-  let d5 = document.getElementById("inpD5").value;
+  let f0 = Number(document.getElementById("inpF0").value);
+  let d1 = Number(document.getElementById("inpD1").value);
+  let d2 = Number(document.getElementById("inpD2").value);
+  let d3 = Number(document.getElementById("inpD3").value);
+  let d4 = Number(document.getElementById("inpD4").value);
+  let d5 = Number(document.getElementById("inpD5").value);
 
   for(let x = 1; x <= 100; x++) {
     let d4_1 = d4 + d5;
@@ -20,23 +20,36 @@ function submit() {
     d3 = d3_1;
     d4 = d4_1;
 
-    let row = table.insertRow(x);
-    let cellF = row.insertCell(0);
-    let cellD1 = row.insertCell(1);
-    let cellD2 = row.insertCell(2);
-    let cellD3 = row.insertCell(3);
-    let cellD4 = row.insertCell(4);
-    let cellD5 = row.insertCell(5);
+    let row = table.insertRow(x + 1);
+    let cellX = row.insertCell(0);
+    let cellF = row.insertCell(1);
+    let cellD1 = row.insertCell(2);
+    let cellD2 = row.insertCell(3);
+    let cellD3 = row.insertCell(4);
+    let cellD4 = row.insertCell(5);
+    let cellD5 = row.insertCell(6);
 
+    cellX.innerHTML = x;
+    cellX.style.textAlign = 'center';
     cellF.innerHTML = f0;
+    cellF.style.textAlign = 'center';
     cellD1.innerHTML = d1;
+    cellD1.style.textAlign = 'center';
     cellD2.innerHTML = d2;
+    cellD2.style.textAlign = 'center';
     cellD3.innerHTML = d3;
+    cellD3.style.textAlign = 'center';
     cellD4.innerHTML = d4;
+    cellD4.style.textAlign = 'center';
     cellD5.innerHTML = d5;
+    cellD5.style.textAlign = 'center';
   } // for 
 }
 
 function reset() {
-  console.log('Reset clicked');
+  let table = document.getElementById("tblFD");
+  let rows = table.rows.length;
+  for(let row = rows - 1; row > 1; row--) {
+    table.deleteRow(row);
+  }
 }
